808e0290e826e671c8743039d79f003fd6eaf045
Tomas Maly <tmaly1980@gmail.com>
2016-09-24 11:36:26 -0400

    improving access  control

--- app/controllers/concerns/authenticate_authorize.rb
+++ app/controllers/concerns/authenticate_authorize.rb
@@ -3,12 +3,12 @@ module Concerns
 		extend ActiveSupport::Concern
 
 		included do
-		  #before_action :authenticate_and_authorize!
+		  before_action :authenticate_and_authorize!
 
 		  def authenticate_and_authorize!
-		  	logger.debug private_methods.join(";")
+		  	logger.debug "PRIMETH="+private_methods.join(";")
 
-		    if private_methods.try(:include?, params[:action]) || private_methods.try(:include?, '*')
+		    if user_methods.try(:include?, params[:action]) || user_methods.try(:include?, '*')
 		      authenticate_user! # devise auth
 		    end
 
@@ -18,28 +18,21 @@ module Concerns
 		  end
 
 		  # For now, assume there are admins and non-admins
-		unless method_defined? :is_authorized?
-			  define_method :is_authorized? do # Per controller/action, check current_user.admin? etc
+			define_method :is_authorized? do # Per controller/action, check current_user.admin? etc
 			    if admin_methods.try(:include?, params[:action]) || admin_methods.try(:include?, '*')
 			      current_user.try(:admin?) || current_user.try(:manager?)
 			    end
 			    true
-			  end
-		end
+			end
 
-		unless method_defined? :private_methods
-			logger.debug "POO PRIVATE"
-		  define_method :private_methods do # If specific controller wants to block something else, it can alter.
-		      ['new','delete','destroy','edit','create']
-		  end
-		end
+			# Override user_methods => super - ['new','create'] # Will allow new/create for anonymous
+			define_method :user_methods do # If specific controller wants to block something else, it can alter.
+			      ['new','delete','destroy','edit','create'] # anon read ok, user only edit
+			end
 
-		unless method_defined? :admin_methods
-		  define_method :admin_methods do # Define per controller; Only admins (NOT volunteers) can go here
-		    []
-		  end
-		end
-
+			define_method :admin_methods do # Define per controller; Only admins (NOT volunteers) can go here
+			    []
+			end
 			
 		end
 
